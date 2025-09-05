2fdc647bf989b96c9399394513876b0892361e3a
Eduardo Melo <eduardo.melo01992@gmail.com>
2025-09-04 22:29:06 -0300

    Category findAll endpoint and configure H2 database

---
+++ backend/src/main/java/com/asmeduardo/dscatalog/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.asmeduardo.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.asmeduardo.dscatalog.entities.Category;
+import com.asmeduardo.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+}
