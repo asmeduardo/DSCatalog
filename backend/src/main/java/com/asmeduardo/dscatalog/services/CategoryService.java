2fa59f0025800c3961fe83bca6820fcdb3187046
Eduardo Melo <eduardo.melo01992@gmail.com>
2025-09-05 14:29:11 -0300

    Insert Category

--- backend/src/main/java/com/asmeduardo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/asmeduardo/dscatalog/services/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.asmeduardo.dscatalog.dtos.CategoryDTO;
+import com.asmeduardo.dscatalog.entities.Category;
 import com.asmeduardo.dscatalog.repositories.CategoryRepository;
 
 import com.asmeduardo.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -26,5 +27,13 @@ public class CategoryService {
     public CategoryDTO findById(Long id) {
         return new CategoryDTO(
                 categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found")));
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category category = new Category();
+        category.setName(dto.getName());
+        category = categoryRepository.save(category);
+        return new CategoryDTO(category);
     }
 }
